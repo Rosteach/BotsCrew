fafe0a18a8c176e4dcdc44a0489026580fe739a7
Ros <rospavlenko@gmail.com>
2016-09-04 22:09:34 +0300

    first commit

---
+++ testSB/src/main/java/com/rosteach/app/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.rosteach.app;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TestSbApplication.class);
+	}
+
+}
---
+++ testSB/src/main/java/com/rosteach/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package com.rosteach.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@RequestMapping("/")
+	public String greeting(){
+		return "Hello world";
+	}
+}
